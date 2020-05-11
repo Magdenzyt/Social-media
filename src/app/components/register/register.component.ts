8ceb3c6e078d68e54bb317ea639ebe9f348dccae
Damian Halatek <melman1996@gmail.com>
2020-05-11 21:03:09 +0200

    Register handling

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { RegisterService } from '../../services/register/register.service'
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+
+  constructor(public rest: RegisterService, private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+  
+    this.registerForm = this.formBuilder.group({
+      username: [''],
+      email: [''],
+      password: ['']
+    });
+  }
+
+  onSubmit(){
+    var formData: any = new FormData();
+
+    formData.append("username", this.registerForm.get("username").value);
+    formData.append("email", this.registerForm.get("email").value);
+    formData.append("password", this.registerForm.get("password").value);
+
+    this.rest.register(formData).subscribe(
+      (response) => {
+        console.log(response);
+        this.router.navigate(["/login"]);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
 }
--- src/app/services/register/register.service.ts
+++ src/app/services/register/register.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(formData){
+    return this.http.post('http://127.0.0.1:8000/register/', formData);
+  }
 }
